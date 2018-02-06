c4774e414665d8ea187375e9614bf7d0bb634a8b
SonyaOrlova <sonya5488@yandex.ru>
2018-02-06 23:28:03 +0300

    Завершает сетку и дек. элементы

---
+++ source/js/popup.js
@@ -0,0 +1,26 @@
+var link = document.querySelector(".product-card__order");
+var popup = document.querySelector(".popup__form");
+var overlay = document.querySelector(".popup__overlay");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+
+  popup.classList.add("popup__form--opened");
+  overlay.classList.add("popup__overlay--opened");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("popup__form--opened")) {
+      popup.classList.remove("popup__form--opened");
+      overlay.classList.remove("popup__overlay--opened")
+    }
+  }
+});
+
+overlay.addEventListener('click', function() {
+  if (popup.classList.contains("popup__form--opened")) {
+    popup.classList.remove("popup__form--opened");
+    overlay.classList.remove("popup__overlay--opened");
+  }
+});
